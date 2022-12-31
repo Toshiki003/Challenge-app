b1d1caa3250299450acc1d0d7650c157ec99363f
関目智史 <zou3road@gmail.com>
2022-12-31 23:18:42 +0900

    Add:ChallengeController内のアクション実装

--- app/controllers/challenge_controller.rb
+++ app/controllers/challenge_controller.rb
@@ -1,10 +1,28 @@
 class ChallengeController < ApplicationController
   def index
+    @challenges = Challenge.all.order(created_at: :desc)
   end
 
   def new
+    @challenge =Challenge.new
+  end
+
+  def create
+    @challenge = Challenge.new(challenge_params)
+    if @challenge.save
+      redirect_to @challenge
+    else
+      render :new
+    end
   end
 
   def show
+    @challenge = Challenge.find(params[:id])
+  end
+
+  private
+  
+  def challenge_params
+    params.require(:challenge).permit(:nickname, :timing, :category)
   end
 end
